23bbfb672918c7d2293e95cd43cb74ef8e30aa73
LioNaMLih <lionamrodriguesdasilva@gmail.com>
2025-01-22 18:57:18 -0300

    lição 1

---
+++ dados_Lição_1.js
@@ -0,0 +1,36 @@
+// Lista de membros e suas notas por lição
+const dadosMembros = [
+  {
+    nome: "lionam",
+    notas: {10
+    
+    }
+  },
+  {
+    nome: "lionara",
+    notas: {9
+    
+    }
+  },
+  {
+    nome: "eduardo",
+    notas: {8
+    
+    }
+  },
+  {
+    nome: "maiko",
+    notas: {7
+    
+    }
+  },
+  {
+    nome: "rafaela",
+    notas: {6
+    
+    }
+  },
+];
+
+// Exportação para possível uso em outro arquivo
+export default dadosMembros;
